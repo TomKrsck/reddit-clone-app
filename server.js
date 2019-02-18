84731ce49b23a6663340deb8a5b9c3be2bc3ab72
TomKrsck <kraswak@gmail.com>
2019-02-18 18:06:31 +1100

    Heroku Configuration changes

--- server.js
+++ server.js
@@ -32,6 +32,7 @@ if (process.env.NODE_ENV === 'production') {
     });
 }
 
-app.listen(process.env.PORT || 5000, () => {
-    var port = server.address().port;
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
     console.log(`Server started on port ${port}`)});
