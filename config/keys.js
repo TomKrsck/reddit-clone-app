56fe81f3b3c9306604705d5bbabe38f94a059d79
TomKrsck <kraswak@gmail.com>
2019-02-19 11:55:17 +1100

    Heroku Implementation

--- config/keys.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    mongoURI: 'mongodb://<Username:Password>@reddit-clone-shard-00-00-glitp.mongodb.net:27017,reddit-clone-shard-00-01-glitp.mongodb.net:27017,reddit-clone-shard-00-02-glitp.mongodb.net:27017/reddit-clone-db?ssl=true&replicaSet=reddit-clone-shard-0&authSource=admin&retryWrites=true'
-}
